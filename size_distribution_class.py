fad23c39a1decc73404811973a55b1ba6c92bc8c
Florian Kleiner <mail@floriankleiner.de>
2020-10-09 15:16:07 +0200

    improving details

--- size_distribution_class.py
+++ size_distribution_class.py
@@ -42,16 +42,17 @@ else:
     print( 'download from https://github.com/kleinerELM/tiff_scaling' )
     sys.exit()
 
-def singeFileProcess(verbose=False):
-    print( "Please select the directory with the source image tiles.", end="\r" )
+def singeFileProcess(analyze_color_name='black', verbose=False):
+
+    print( "Please select the directory with the source image tiles.")#, end="\r" )
     filepath = filedialog.askopenfilename( title='Please select the reference image', filetypes=[("Tiff images", "*.tif;*.tiff")] )
     file_name, file_extension = os.path.splitext( filepath )
     file_name = os.path.basename(file_name)
 
-    return size_distribution(os.path.dirname(filepath), file_name, file_extension, verbose=verbose)
+    return size_distribution(os.path.dirname(filepath), file_name, file_extension, analyze_color_name=analyze_color_name, verbose=verbose)
 
 class size_distribution():
-
+    progressFactor = 500
     image_count = 0
     tile_width = 0
     tile_height = 0
@@ -60,11 +61,19 @@ class size_distribution():
     scaling = es.getEmptyScaling()
     pore_diameter_limit = 100 #nm
 
-    materialColor = 255
-    poreColor = 0
+    black = 0
+    white = 255
 
     folder = ''
     output_folder = ''
+
+    def set_color_to_be_analyzed( self, color_name ):
+        if color_name == "white":
+            self.analyze_Color = self.white
+            self.ignore_Color = self.black
+        else:
+            self.analyze_Color = self.black
+            self.ignore_Color = self.white
 
     def getPoreDiameter( self, area ):
         return ( math.sqrt( area /math.pi )*2 )
@@ -83,11 +92,18 @@ class size_distribution():
         radius = diameter/2
         return (4*math.pi*(radius**2))
 
+    def get_scaled_width(self):
+        return self.tile_width*self.scaling['x']
+
+    def get_scaled_height(self):
+        return self.tile_height*self.scaling['y']
+
     def check_tile_dimension(self, height, width):
         if self.tile_width + self.tile_height == 0:
             self.tile_width = width
             self.tile_height = height
             self.tile_area = height*width
+            print('size: {} x {} {} / {} x {} px '.format(self.get_scaled_width(),self.get_scaled_height(),self.scaling['unit'],width,height))
             return True
         else:
             if self.tile_width == width and self.tile_height == height:
@@ -97,38 +113,33 @@ class size_distribution():
                 exit()
         return False
 
-    def load_binary_image(self, filename):
-        self.scaling = es.autodetectScaling( filename, self.folder )
-        img = cv2.imread( self.folder + os.sep + filename, cv2.IMREAD_GRAYSCALE )
+    def load_binary_image(self):
+        self.scaling = es.autodetectScaling( self.filename, self.folder )
+        self.img = cv2.imread( self.folder + os.sep + self.filename, cv2.IMREAD_GRAYSCALE )
 
-        height, width = img.shape[:2]
-
-        if img is None:
-            print( 'Error loading {}'.format(filename))
+        if self.img is None:
+            print( 'Error loading {}'.format(self.filename))
             exit()
 
         #binarizes an image
         #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        min_val = int( (self.materialColor+1)/2 ) if self.materialColor > self.poreColor else self.poreColor
-        max_val = int( (self.poreColor+1)/2 ) if self.poreColor > self.materialColor else self.materialColor
-        _, img = cv2.threshold(img, min_val, max_val, cv2.THRESH_BINARY)
-        return img
+        min_val = self.black #int( (self.ignore_Color+1)/2 ) if self.ignore_Color > self.analyze_Color else self.analyze_Color
+        max_val = int( (self.white+1)/2 ) #int( (self.analyze_Color+1)/2 ) if self.analyze_Color > self.ignore_Color else self.ignore_Color
+        _, self.thresh_img = cv2.threshold(self.img, min_val, max_val, cv2.THRESH_BINARY)
 
     def get_file(self, file_name, file_extension, process_position=None, verbose=False ):
         processID = ' '
         if process_position != None: processID = " #{}: ".format(process_position)
-        filename = file_name + file_extension
-        img = self.load_binary_image(filename)
+        self.filename = file_name + file_extension
+        self.load_binary_image()
 
-        if verbose: print( '{}Analysing {}'.format(processID, filename) )
+        if verbose: print( '{}Analysing {}'.format(processID, self.filename) )
 
-        height, width = img.shape[:2]
+        height, width = self.img.shape[:2]
 
         if self.check_tile_dimension(height, width):
             new_row = {'filename':file_name, 'height':height, 'width':width }
         self.analyzed_files_DF = self.analyzed_files_DF.append(new_row, ignore_index=True)
-
-        return img
 
     def process_result_row( self, file_index, diameter=0, area=0 ):
         result_row = False
@@ -156,7 +167,6 @@ class size_distribution():
         contourCount = len(contours)
         file_index = int(len(self.analyzed_files_DF)-1)
         emptyArea = 0
-        progressFactor = 500
 
         startTime = int(round(time.time() * 1000))
         for i in range(0, contourCount):
@@ -164,82 +174,71 @@ class size_distribution():
             result_row = self.process_result_row( file_index, area=area )
             if result_row != False:
                 self.psd_df = self.psd_df.append(result_row, ignore_index=True)
-                if i % progressFactor == 0: print( "{}...processing particle #{}/{}".format(processID, i, contourCount ))#, end="\r")
+                if i % self.progressFactor == 0 and verbose: print( "{}...processing particle #{}/{}".format(processID, i, contourCount ))#, end="\r")
             else:
                 emptyArea += 1
         if verbose:
             print( '{}Analysed particles: {}, ignored 0-values: {}, finished in {} ms'.format(processID, contourCount-emptyArea, emptyArea, int(round(time.time() * 1000)) - startTime) )
             #self.psd_df.to_csv( self.output_folder + file_name + '_pores.csv', index=False )
-            print(self.psd_df)
-
+            #print(self.psd_df)
 
     def process_directional_CLD( self, img, direction, processID = ' ', verbose=False):
-        #global materialColor
-        #global ignoreBorder
-        #global minLength
-
         file_index = int(len(self.analyzed_files_DF)-1)
 
+        ignoreBorder = True
+        minLength = 1
+
+        #process variables
         lineCount = 0
         lastValue = -1      # var to save the last value
         lastChangedPos = -1   # var to save the last position
-        #width, height = img.size
-        #imageArea = width * height
         usedImageArea = 0
         fullPoreArea = 0
-        pixelWidth = self.scaling['x']
-        pixelHeight = self.scaling['y']
-        ignoreBorder = True
 
-        minLength = 1
-        resultCSV = ''
         if verbose: print( '  processing {} cord length distribution'.format(direction) )#, end='' )
         startTime = int(round(time.time() * 1000))
         if ( direction == 'horizontal' ):
             for y in range(self.tile_height):
-                if ( y % 100 == 0 and verbose): print('  ... line {} of {}'.format(y, self.tile_height), end="\r")
+                if y % self.progressFactor == 0 and verbose: print('  ... line {} of {}'.format(y, self.tile_height))#, end="\r")
                 for x in range(self.tile_width):
                     value = img[y,x]#.getpixel((x,y))
                     borderReached = (x == self.tile_width-1)
-                    if ( value != self.materialColor ): fullPoreArea +=1
+                    if ( value != self.ignore_Color ): fullPoreArea +=1
                     if ( value != lastValue or borderReached ):
                         isBorder = ( lastChangedPos < 0 or borderReached )
                         length = x - lastChangedPos
-                        if ( (value != lastValue and value == self.materialColor) or ( lastChangedPos > 0 and value != self.materialColor and isBorder and not ignoreBorder ) ): # if materialColor appears, a completed void line is detected
+                        if ( (value != lastValue and value == self.ignore_Color) or ( lastChangedPos > 0 and value != self.ignore_Color and isBorder and not ignoreBorder ) ): # if ignore_Color appears, a completed void line is detected
                             if ( length != self.tile_width and length > minLength ):
                                 lineCount += 1
                                 usedImageArea += length
                                 result_row = self.process_result_row( file_index, diameter=length )
                                 self.cld_df = self.cld_df.append(result_row, ignore_index=True)
-                                #resultCSV += str(x) +"	" + str(y)  +"	" + str(lastChangedPos)+"	" + str( lineCount ) + "	" + str( pixelWidth * length ) + "\n"#+ "	" + str( length/imageArea ) + "\n"
-                                #resultCSV += str( lineCount ) + "	" + str( pixelWidth * length ) + "\n"#+ "	" + str( length/imageArea ) + "\n"
                         if ( borderReached ):
                             lastChangedPos = -1
-                            lastValue = self.materialColor
+                            lastValue = self.ignore_Color
                         else:
                             lastChangedPos = x
                     lastValue = value
 
         elif ( direction == 'vertical' ):
             for x in range(self.tile_width):
-                if ( x % 100 == 0 and verbose ): print('  ... line {} of {}'.format(x, self.tile_width), end="\r")
+                if x % self.progressFactor == 0 and verbose: print('  ... line {} of {}'.format(x, self.tile_width))#, end="\r")
                 for y in range(self.tile_height):
                     value = img[y,x]#.getpixel((x,y))
                     borderReached = (y == self.tile_height-1)
-                    if ( value != self.materialColor ): fullPoreArea +=1
+                    if ( value != self.ignore_Color ): fullPoreArea +=1
                     if ( value != lastValue or borderReached ):
                         isBorder = ( lastChangedPos < 0 or borderReached )
                         length = y - lastChangedPos
-                        if ( (value != lastValue and value == self.materialColor) or ( lastChangedPos > 0 and value != self.materialColor and isBorder and not ignoreBorder ) ): # if materialColor appears, a completed void line is detected
+                        if ( (value != lastValue and value == self.ignore_Color) or ( lastChangedPos > 0 and value != self.ignore_Color and isBorder and not ignoreBorder ) ): # if ignore_Color appears, a completed void line is detected
                             if ( length != self.tile_height and length > minLength ):
                                 lineCount += 1
                                 usedImageArea += length
                                 result_row = self.process_result_row( file_index, diameter=length )
                                 self.cld_df = self.cld_df.append(result_row, ignore_index=True)
-                                #resultCSV += str( lineCount ) + "	" + str( pixelHeight * length ) + "\n"
                         if ( borderReached ):
                             lastChangedPos = -1
-                            lastValue = self.materialColor
+                            lastValue = self.ignore_Color
                         else:
                             lastChangedPos = y
                     lastValue = value
@@ -249,12 +248,6 @@ class size_distribution():
         if verbose:
             print( "   {} lines measured in {} ms".format(lineCount, int(round(time.time() * 1000)) - startTime), end='' )
             print( "   {:.2f} of {:.2f} area-% were taken into account".format(100/self.tile_area*usedImageArea, 100/self.tile_area*fullPoreArea) )
-        #    #print( str( usedImageArea ) + '  ' + str( fullPoreArea ) )
-        #    headerLine = "lineCount" + "	" + "length [nm]" + "\n"#+ "	" + "volume fraction" + "\n"
-        #    resultFile = open(self.output_folder + filename + "." + direction + ".csv","w")
-        #    resultFile.write( headerLine + resultCSV )
-        #    resultFile.close() #to change file access modes
-        #return resultCSV
 
     def processCLD( self, img, processID=' ', process_position=None, verbose=False ):
         self.process_directional_CLD(img, 'horizontal', verbose=verbose)
@@ -268,7 +261,7 @@ class size_distribution():
 
         chords_y = porespy.filters.apply_chords(img, axis=1, spacing=1, trim_edges=True)
         cld_y = porespy.metrics.chord_length_distribution(chords_y, bins=100, log=True)
-        print(len(chords_x), len(chords_y))
+        #print(len(chords_x), len(chords_y))
         #print(chords_x, chords_y)
         print('finished in {} ms'.format(int(round(time.time() * 1000)) - startTime))
         #fig, (ax0, ax1) = plt.subplots( ncols=2, nrows=1, figsize=(20,10) )
@@ -297,23 +290,28 @@ class size_distribution():
 
     def clean_histogram(self, histogram, bins, power=1):
         if power > 1:
-            pow_bins = bins
+            pow_bins = []#bins
             maxval = numpy.amax(bins)
             cleaned_hist = []
             pos = 0
             deleted = 0
             for value in histogram:
-                if value == 0:
-                    pow_bins = numpy.delete(pow_bins, pos-deleted)
-                    deleted += 1
-                else:
+                if value > 0:
+                    pow_bins.append(bins[pos])
                     cleaned_hist.append(value)
-
                 pos += 1
-        print(len(pow_bins), len(cleaned_hist))
-        print((pow_bins), (cleaned_hist))
-        return histogram, bin
+            #print(len(pow_bins), len(cleaned_hist))
+            #print((pow_bins), (cleaned_hist))
+        else:
+            pow_bins = bins
+            cleaned_hist = histogram
+        return cleaned_hist, [bins[0]]+pow_bins
 
+    def get_basic_values(self, column, df):
+        col_max = df[column].max
+        col_mean = df[column].mean
+        col_median = df[column].median
+        return col_max, col_mean, col_median
 
     def get_sum_list(self, value_list):
         sums = [0]
@@ -324,26 +322,28 @@ class size_distribution():
     def get_values_above(self, series, value):
         return numpy.where( list(series) > value )
 
-    def __init__(self, folder, file_name, file_extension, verbose=False):
+    def __init__(self, folder, file_name, file_extension, analyze_color_name='black', verbose=False):
+        self.set_color_to_be_analyzed( analyze_color_name )
+
         self.folder = folder
         self.output_folder = self.folder + os.sep + 'CSV' + os.sep
         self.target_folder = os.path.abspath(os.path.join(self.folder, os.pardir))
+
         self.analyzed_files_DF = pd.DataFrame(columns = ['filename' , 'height', 'width'])
         dataframe_columns = ['file_index', 'diameter', 'area', 'surface', 'volume']
         self.psd_df = pd.DataFrame(columns = dataframe_columns)
         self.cld_df = pd.DataFrame(columns = dataframe_columns)
 
+        self.get_file(file_name, file_extension, verbose=verbose)
 
-        img = self.get_file(file_name, file_extension, verbose=verbose)
-
         print('-'*50)
-        self.processPSD(img, verbose=verbose)
+        self.processPSD(self.thresh_img, verbose=verbose)
         print('-'*50)
-        self.get_porespy_CLD(img)
+        self.get_porespy_CLD(self.thresh_img)
         print('-'*50)
-        self.process_directional_CLD(img, 'horizontal', verbose=verbose)
+        self.process_directional_CLD(self.thresh_img, 'horizontal', verbose=verbose)
         print('-'*50)
-        self.process_directional_CLD(img, 'vertical', verbose=verbose)
+        self.process_directional_CLD(self.thresh_img, 'vertical', verbose=verbose)
         #self.processCLD(img, verbose=verbose)
         print('-'*50)
 
